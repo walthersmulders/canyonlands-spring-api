5105ed2ed6e05814f96b185d485af5c7bc07bc14
Walther Smulders <26358873+walthersmulders@users.noreply.github.com>
2024-03-30 00:55:03 +0200

    feat: Keycloak and security config
    — dockerfile configuration added for keycloak with realm import functionality.
    — preconfigured realm added.
    — oauth client dependency added.
    — README updated with keycloak instructions and notes.
    — properties updated to include security and keycloak config.
    — initial security configuration for endpoints.

---
+++ src/main/java/com/walthersmulders/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.walthersmulders.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
