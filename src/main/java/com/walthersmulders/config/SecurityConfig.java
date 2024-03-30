e51240741a6323b845746575e9fa976f03213bf9
Walther Smulders <26358873+walthersmulders@users.noreply.github.com>
2024-03-30 14:53:13 +0200

    feat: method level security
    — updated preconfigured keycloak realm to include client roles and assigned to default user
    — updated readme document to include user roles section
    — updated security config with authority mapper for jwt token, and enable method level security annotation

--- src/main/java/com/walthersmulders/config/SecurityConfig.java
+++ src/main/java/com/walthersmulders/config/SecurityConfig.java
@@ -2,15 +2,26 @@ package com.walthersmulders.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,5 +35,26 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+            Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+
+            Map<String, List<String>> clientRoleMap = (Map<String, List<String>>) resourceAccess.get("canyonlands");
+
+            List<String> clientRoles = clientRoleMap.get("roles");
+
+            return List.of(clientRoles.stream()
+                                      .map(SimpleGrantedAuthority::new)
+                                      .toArray(GrantedAuthority[]::new));
+        };
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
     }
 }
