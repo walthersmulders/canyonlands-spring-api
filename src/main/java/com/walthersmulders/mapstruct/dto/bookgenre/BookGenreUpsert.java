dc93fd6dd8c39144a72253f99defc72b5360c8cd
Walther Smulders <26358873+walthersmulders@users.noreply.github.com>
2024-03-31 18:49:20 +0200

    chore: rename BookGenreNoID to BookGenreUpsert

--- src/main/java/com/walthersmulders/mapstruct/dto/bookgenre/BookGenreUpsert.java
+++ src/main/java/com/walthersmulders/mapstruct/dto/bookgenre/BookGenreUpsert.java
@@ -3,7 +3,6 @@ package com.walthersmulders.mapstruct.dto.bookgenre;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
-// TODO :: Rename to BookGenreUpsert
 public record BookGenreUpsert(
         @NotEmpty @Size(min = 1, max = 255) String genre,
         @NotEmpty @Size(min = 1, max = 500) String subGenre
