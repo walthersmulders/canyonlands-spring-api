23fc9d46d3ce032c35c3312a2ffdf5b459d7621f
Walther Smulders <26358873+walthersmulders@users.noreply.github.com>
2024-06-04 21:47:08 +0200

    feat (music): artist service and repository implementation

--- src/main/java/com/walthersmulders/persistence/repository/music/ArtistRepository.java
+++ src/main/java/com/walthersmulders/persistence/repository/music/ArtistRepository.java
@@ -2,10 +2,35 @@ package com.walthersmulders.persistence.repository.music;
 
 import com.walthersmulders.persistence.entity.music.ArtistEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ArtistRepository extends JpaRepository<ArtistEntity, UUID> {
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END " +
+           "FROM ArtistEntity a " +
+           "WHERE a.firstName = :firstName " +
+           "AND a.lastName = :lastName")
+    boolean exists(
+            @Param(value = "firstName") String firstName,
+            @Param(value = "lastName") String lastName
+    );
+
+    @Query("SELECT a " +
+           "FROM ArtistEntity a " +
+           "LEFT JOIN FETCH a.albums")
+    List<ArtistEntity> fetchAllWithAlbums();
+
+    @Query("SELECT a " +
+           "FROM ArtistEntity a " +
+           "LEFT JOIN FETCH a.albums " +
+           "WHERE a.artistID = : artistID")
+    Optional<ArtistEntity> fetchArtistWithAlbums(
+            @Param(value = "artistID") UUID artistID
+    );
 }
--- src/main/java/com/walthersmulders/service/music/ArtistAlbumService.java
+++ src/main/java/com/walthersmulders/service/music/ArtistAlbumService.java
@@ -1,18 +1,23 @@
 package com.walthersmulders.service.music;
 
+import com.walthersmulders.exception.EntityExistsException;
+import com.walthersmulders.exception.EntityNotFoundException;
 import com.walthersmulders.mapstruct.dto.artist.Artist;
 import com.walthersmulders.mapstruct.dto.artist.ArtistUpsert;
 import com.walthersmulders.mapstruct.dto.artist.ArtistWithAlbums;
 import com.walthersmulders.mapstruct.mapper.AlbumMapper;
 import com.walthersmulders.mapstruct.mapper.ArtistMapper;
+import com.walthersmulders.persistence.entity.music.ArtistEntity;
 import com.walthersmulders.persistence.repository.genre.GenreMusicRepository;
 import com.walthersmulders.persistence.repository.music.AlbumRepository;
 import com.walthersmulders.persistence.repository.music.ArtistRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+
+import static java.util.Map.entry;
 
 @Service
 @Slf4j
@@ -43,26 +48,114 @@ public class ArtistAlbumService {
     }
 
     public Artist createArtist(ArtistUpsert artist) {
-        return null;
+        log.info("Creating artist");
+
+        boolean exists = artistRepository.exists(artist.firstName(), artist.lastName());
+
+        if (exists) {
+            log.error("Artist already exists");
+
+            throw new EntityExistsException(
+                    ARTIST, Map.ofEntries(
+                    entry("first name", artist.firstName()),
+                    entry("last name", artist.lastName())
+            ));
+        }
+
+        ArtistEntity artistEntity = artistMapper.artistUpsertToEntity(artist);
+
+        artistRepository.save(artistEntity);
+
+        log.info("Artist created with id {}", artistEntity.getArtistID());
+
+        return artistMapper.entityToArtist(artistEntity);
     }
 
     public List<Artist> getArtists() {
-        return null;
+        log.info("Getting all artists without links");
+
+        List<ArtistEntity> artists = artistRepository.findAll();
+
+        log.info("Found {} artists", artists.size());
+
+        return artists.isEmpty() ? Collections.emptyList()
+                                 : artists.stream()
+                                          .map(artistMapper::entityToArtist)
+                                          .toList();
     }
 
+    @Transactional(readOnly = true)
     public List<ArtistWithAlbums> getArtistsWithAlbums() {
-        return null;
+        log.info("Getting all artists with albums");
+
+        List<ArtistEntity> artistsWithLinks = artistRepository.fetchAllWithAlbums();
+
+        log.info("Found {} artists", artistsWithLinks.size());
+
+        return artistsWithLinks.isEmpty() ? Collections.emptyList()
+                                          : artistsWithLinks.stream()
+                                                            .map(artistMapper::entityToArtistWithAlbums)
+                                                            .toList();
     }
 
     public Artist getArtist(UUID id) {
-        return null;
+        log.info("Getting artist with artistID {}", id);
+
+        ArtistEntity artist = artistRepository.
+                findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        ARTIST,
+                        Map.of(ARTIST_ID, id.toString())
+                ));
+
+        return artistMapper.entityToArtist(artist);
     }
 
+    @Transactional(readOnly = true)
     public ArtistWithAlbums getArtistWithAlbums(UUID id) {
-        return null;
+        log.info("Getting artist with albums with artistID {}", id);
+
+        ArtistEntity artistWithAlbums = artistRepository
+                .fetchArtistWithAlbums(id)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        ARTIST,
+                        Map.of(ARTIST_ID, id.toString())
+                ));
+
+        return artistMapper.entityToArtistWithAlbums(artistWithAlbums);
     }
 
     public void updateArtist(UUID id, ArtistUpsert artist) {
+        log.info("Updating artist with artistID {}", id);
 
+        Optional<ArtistEntity> existingArtist = artistRepository.findById(id);
+
+        if (existingArtist.isEmpty()) {
+            log.error("Artist with id {} not found", id);
+
+            throw new EntityNotFoundException(
+                    ARTIST,
+                    Map.of(ARTIST_ID, id.toString())
+            );
+        }
+
+        log.info("Check if incoming object has the same fields as existing");
+
+        if (existingArtist.get().checkUpdateDtoEqualsEntity(artist)
+            && Objects.equals(existingArtist.get().getAdditionalName(), artist.additionalName())
+        ) {
+            log.info("Incoming object has the same fields as existing, no need to update");
+        } else {
+            log.info("Incoming object has different fields as existing, updating");
+
+            ArtistEntity updatedArtist = artistMapper.artistEntityUpdateMerge(
+                    existingArtist.get(),
+                    artist
+            );
+
+            artistRepository.save(updatedArtist);
+
+            log.info("Artist updated with artistID {}", id);
+        }
     }
 }
